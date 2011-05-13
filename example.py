252f4390188832b2c47bbe4598c22343548cda2c
Rui Carmo <rui.carmo@gmail.com>
2011-05-13 23:29:19 +0100

    Added basic example

---
+++ example.py
@@ -0,0 +1,19 @@
+from airvideo.client import AVClient
+import os
+
+
+def main():
+  # connect
+  c = AVClient('locahost', password="password")
+
+  # look for our folder
+  path = filter(lambda x: x.name == 'my videos', c.browse()[0].contents)[0].path
+
+  # get data for a subfolder
+  print map(lambda x: x.detail, filter(lambda x: not x.isFolder,c.browse(path + "/sample")))
+
+  # get queue info
+  print c.get_queue()
+
+if __name__ == '__main__':
+  main()
